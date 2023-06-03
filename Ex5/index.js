5c80f4128c9f9c2f9a4eff42a1a3e75940b71ce2
Enghav <enghav12@gmail.com>
2023-06-03 08:59:25 +0700

    add

---
+++ Ex5/index.js
@@ -0,0 +1,7 @@
+function togglePassword() {
+    var passwordField = document.getElementById("password");
+    if (passwordField.type === "password") {
+      passwordField.type = "text" && passwordField.type = "password";
+    } 
+  }
+  
